d2f964eabd51bbaf4db367a83128829b6a0bb503
hsy <huangsy13@gmail.com>
2016-12-27 16:56:58 +0800

    update

---
+++ 分享到/scripts/mumayi_top.js
@@ -0,0 +1,27 @@
+function mmymytop() {
+
+    var imumayitop = eval(function (p, a, c, k, e, r) {
+        e = function (c) {
+            return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+        };
+
+        while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+        return p
+    }('8 Z={1v:!!(9.1w&&!9.x),1x:!!9.x,1y:y.z.J("1z/")>-1,10:y.z.J("10")>-1&&y.z.J("1A")==-1,11:!!y.z.1B(/1C.*1D.*1E/)};8 K=Z.11;8 k=d;8 l;4($.q.L){l=0}e{l=M}8 1F=(l==0)?0:1G;8 g=d;8 1H=d;8 12=13 14();6 1I(a){15=13 14();N=(15.16()-12.16());4(17){O=(a)?a+" ":"";O+=(N<1J)?"1K =>":"1L 1M =>";17.1N((O+N))}}4(K){$("#18").m({1O:"1P"});$("#18").m({"1Q-1R":"1S"})}6 19(b,a){r=b.1a("/");r=r[r.P-1].1a(".");1b=r[0]+a;Q 1b}6 1T(a){a="#"+a;$(a)[0].1c=1U+19($(a)[0].1c,".1V")}6 1d(){8 b=0,a=0;4(1W(9.1e)=="1X"){a=9.1e;b=9.1Y}e{4(c.h&&(c.h.A||c.h.7)){a=c.h.7;b=c.h.A}e{4(c.n&&(c.n.A||c.n.7)){a=c.n.7;b=c.n.A}}}Q a}6 1f(a){R=0;1Z(a){20"S":R=0;1g}B=$.q.x?$("s"):$("s, h");B.T({7:R},"1h")}8 C=d;8 j=[0,0,0,1];8 1i=21;8 1j=22;8 U=1k;8 D=d;6 V(){23(i=0;i<j.P;i++){4(j[i]==1){j[i]=0;4(!D){4((i+2)<j.P){j[i+1]=1}e{j[0]=1;D=t}}e{4((i-1)<0){j[1]=1}e{j[i-1]=1;D=d}}1g}}$("#7 .u-2").m({"W-X":"-"+(1j+(i*1i))+"1l 1m",f:"v"});C=Y("V()",U)}6 24(){4(!K){$("#7 1n.u-3").25(6(){4($.q.L){o.E.F[0].p.f="v"}e{$(o.E.F[0]).1o().1p(M,1)}},6(){4(k||g){Q}4($.q.L){o.E.F[0].p.f="w"}e{$(o.E.F[0]).1o().1p(M,0)}});$("#7 1n.u-3").26(6(){g=t;$("#7 .u-2").m({"W-X":"-27 0",f:"v"});B=$.q.x?$("s"):$("s, h");C=Y("V()",U);B.T({7:0},"1h",6(){g=d;4(!k){k=t;1q=$("#7")[0].28+29;$("#7").T({"1r-S":"-="+1q+"1l"},2a,6(){1s()})}})});9.2b=6(){4((!g)&&(!k)){2c=$("h")[0];2d=$("s")[0];4(9.1t){G=9.1t;H=9.2e}e{G=c.n.2f;H=1d()}1u=$("h")[0];I=$("#7")[0];4((1u)&&(I)){4((I.p.f=="w")&&((G*1.5)<H)){g=t;$("#7").2g(l,6(){g=d;o.p.f="v"})}4((I.p.f=="v")&&((G*1.5)>H)){g=t;$("#7").2h(l,6(){g=d;o.p.f="w"})}}}}}e{Y(6(){9.1f(0,1)},1k)}}6 1s(){$("#7 .u-2").m({"W-X":"-2i 1m",f:"w"});$("#7").m({"1r-S":"-2j",f:"w"});k=d;2k(C)};', 62, 145, '||||if||function|scrollTop|var|window|||document|false|else|display|scroll_animate|body||rocketFireState|upAnimate|anim_time|css|documentElement|this|style|browser|preNewSrc|html|true|level|block|none|opera|navigator|userAgent|scrollLeft|op|rocketFireTimer|toLeftFireAnimation|parentNode|children|wind_height|wind_scroll|scrollBtn|indexOf|mobileSafari|msie|500|loadTime|logStr|length|return|NewDocumentHeight|top|animate|rocketFireAnimateTime|rocketFireAnimate|background|position|setTimeout|browser_detect|Gecko|MobileSafari|domStart|new|Date|domStop|getTime|console|wrapper|getNewSrc|split|newSrc|src|getScrollY|pageYOffset|scrollTo|break|slow|rocketFireFrameLength|rocketFireFrameStart|100|px|0px|div|stop|fadeTo|thisTop|margin|resetScrollUpBtn|innerHeight|elem|IE|attachEvent|Opera|WebKit|AppleWebKit|KHTML|match|Apple|Mobile|Safari|anim_time_short|350|menuSelected|culculateDomRedy|1000|cache|full|refresh|log|overflow|hidden|min|height|1000px|setImgSrc|project_path|svg|typeof|number|pageXOffset|switch|case|149|298|for|initScrollTop|hover|click|298px|offsetTop|250|300|onscroll|body_elem|window_elem|scrollY|clientHeight|fadeIn|fadeOut|149px|125px|clearTimeout'.split('|'), 0, {}))
+
+    /* !important*/
+    function immytop() {
+        initScrollTop();
+    }
+
+    immytop();
+}
+mmymytop();
+
+
+
+
+
+
+
+
